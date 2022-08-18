2c17ddf66c1da4ff2b04eb9b2e5d62061a0379d3
Pedro Almodovar <pedro.pinto@contabilizei.com.br>
2022-08-18 16:24:47 -0300

    Preparando ambiente

---
+++ src/components/Questionario/index.tsx
@@ -0,0 +1,16 @@
+function retornaIndices(){
+    
+}
+
+function Questionario() {
+    const variaveis = ['nome', 'idade', 'cidade'];
+
+    return (
+        <div>
+            Nome: <input type="text" name="Nome: " id="item"/>
+        </div>
+    )
+
+}
+
+export default Questionario;
